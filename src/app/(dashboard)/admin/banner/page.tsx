bb17847f01c7d62f945bb04211819e69c6efba3d
lirhzaaa <mhmdazhrilnmldn@gmail.com>
2026-01-23 20:40:31 +0700

    feat: add suspense in page banner

--- src/app/(dashboard)/admin/banner/page.tsx
+++ src/app/(dashboard)/admin/banner/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import Banner from "@/src/components/views/Admin/Banner";
+import { Spinner } from "@heroui/react";
+import { Suspense } from "react";
 
 const CategoryPage = () => {
-    return <Banner />
+    return (
+        <Suspense fallback={<Spinner color="primary" size="sm" className="mx-auto mt-4" />}>
+            <Banner />
+        </Suspense>
+    )
 };
 
 export default CategoryPage;
